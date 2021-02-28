050882b8935e77dbcc09f759a080524d8124bb27
azbroja <azbroja@outlook.com>
2021-02-28 15:56:43 +0100

    add flask api

---
+++ resources/item.py
@@ -0,0 +1,70 @@
+from typing import Text
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from models.item import ItemModel
+import requests
+
+
+    
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('name',
+                        type=Text,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    
+    @jwt_required()
+    def get(self):
+        data = Item.parser.parse_args()
+        item = ItemModel.find_by_name(data['name'])
+        if item:
+            return item.json()
+        return {'message': 'Item not found'}, 404
+
+    def post(self):
+        data = Item.parser.parse_args()
+        if ItemModel.find_by_name(data['name']):
+            return {'message': "An item with name '{}' already exists.".format(data['name'])}, 400
+
+        url = f"http://api.ipstack.com/{data['name']}?access_key=75c5dc00c321ae103780a846fe59e797&format=1"
+        response = requests.get(url)
+        response_data = response.json()
+
+        api_data = {"lon": response_data['longitude'], "lat": response_data['latitude']}
+
+        item = ItemModel(data['name'], **api_data)
+
+        try:
+            item.save_to_db()
+        except:
+            return {"message": "An error occurred inserting the item."}, 500
+
+        return item.json(), 201
+
+    def delete(self):
+        data = Item.parser.parse_args()
+        item = ItemModel.find_by_name(data['name'])
+        if item:
+            item.delete_from_db()
+            return {'message': 'Item deleted.'}
+        return {'message': 'Item not found.'}, 404
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = ItemModel.find_by_name(data['name'])
+
+        if item:
+            item.lon = data['lon']
+            item.lat = data['lat']
+        else:
+            item = ItemModel(**data)
+
+        item.save_to_db()
+
+        return item.json()
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}
