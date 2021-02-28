8f69d0b4295e276ee9ac1762adaa9082bc5db74a
azbroja <azbroja@outlook.com>
2021-02-28 18:39:44 +0100

    set enviroment for heroku

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_jwt import JWT
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
+from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -11,6 +12,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'andrzej'
 api = Api(app)
+
+db.init_app(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 jwt = JWT(app, authenticate, identity)
 
